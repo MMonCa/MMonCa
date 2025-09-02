2e0a5a62a75e4d56af37b08878b007bf010886ca
Balazs Bamer <bamer@mmora.iue.tuwien.ac.at>
2025-09-02 18:23:44 +0200

    Bugfix for Issue 47.

--- src/lkmc/LatticeAtomDEpi.cpp
+++ src/lkmc/LatticeAtomDEpi.cpp
@@ -80,7 +80,7 @@ void LatticeAtomDEpi::perform (Kernel::SubDomain *pSub, unsigned eventType)
 		ERRORMSG("LatticeAtomDEpi::Unknown event " << eventType);
 
 	for(map<int, LatticeAtomDiamond *>::iterator it=toUpdate.begin(); it!=toUpdate.end(); ++it)
-		_pDomain->_pRM->update(it->second, static_cast<LatticeAtomDEpi *>(it->second)->_pElement);
+		_pDomain->_pRM->update(it->second, it->second->getElement());
 	if(_pElement->getNonCrystallineLA() == 0)
 		_pDomain->_pLKMC->cleanLKMCAtoms(_pElement, MOD_Epitaxy);
 }
@@ -232,7 +232,7 @@ float LatticeAtomDEpi::getRate(unsigned eventType, float kT) const
 	unsigned neigStates[3] = { 0, 0, 0 }; //array with neighboring states
 	for(unsigned i=0; i<FIRSTN; ++i)
 		if(_neighbors[i])
-			neigStates[static_cast<LatticeAtomDEpi *>(_neighbors[i])->_state]++;
+			neigStates[_neighbors[i]->getState()]++;
 	if( (eventType <    MAX_EPI_GASES && neigStates[LS_PERFORMED] == 0) ||
 	    (eventType == 1+MAX_EPI_GASES && neigStates[LS_AVAILABLE] == 0))
 		return 0;
@@ -340,7 +340,7 @@ double LatticeAtomDEpi::getMigRate(float tempEner, float kT) const
 	float returnRate = 0;
 	_rateMigs = 0;
 	for(unsigned i=0; i<SECONDN; ++i)
-		if(_neighbors[i] && static_cast<LatticeAtomDEpi *>(_neighbors[i])->_state == LS_AVAILABLE)
+		if(_neighbors[i] && _neighbors[i]->getState() == LS_AVAILABLE)
 		{
 			_atomMig[_rateMigs] = static_cast<LatticeAtomDEpi *>(_neighbors[i]); //candidate for "moving"
 			LatticeAtomDEpi *candidate = _atomMig[_rateMigs];
